d42ee81cd613c206063b59047fa220aa47925f6d
0xneves <asguilift@hotmail.com>
2023-12-06 18:17:37 -0300

    Initial Commit
    Adding common hardhat files

---
+++ hardhat.config.ts
@@ -0,0 +1,32 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import dotenv from "dotenv";
+dotenv.config();
+
+const {
+	DEPLOYER_PRIVATE_KEY,
+	SEPOLIA_RPC_URL,
+	ETHERSCAN_API_KEY
+} = process.env;
+
+const config: HardhatUserConfig = {
+	solidity: "0.8.17",
+	etherscan: {
+		apiKey: `${ETHERSCAN_API_KEY}`,
+	},
+	networks: {
+		/**
+		 * @dev Testnets
+		 */
+		sepolia: {
+			url: `${SEPOLIA_RPC_URL}`,
+			accounts: [`${DEPLOYER_PRIVATE_KEY}`],
+		},
+	},
+	defaultNetwork: "hardhat",
+	gasReporter: {
+		enabled: true,
+	}
+};
+
+export default config;
