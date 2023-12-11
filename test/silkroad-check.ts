b919aee576231e7d3a8a07bfb3ed69e14c1d784b
0xneves <asguilift@hotmail.com>
2023-12-10 21:10:56 -0300

    feat: checkage script of view functions

--- test/silkroad-check.ts
+++ test/silkroad-check.ts
@@ -1,39 +1,57 @@
 import { ethers } from "hardhat";
-import { Swap, composeSwap } from "./SwapFactory";
-import { blocktimestamp } from "../scripts/utils";
+import dotenv from "dotenv";
+dotenv.config();
+
+const {
+  DEPLOYER_PRIVATE_KEY,
+  MUMBAI_RPC_URL,
+  BSCTESTNET_RPC_URL,
+  OVERSWAP_MUMBAI,
+  OVERSWAP_BNB,
+} = process.env;
 
 async function main() {
-  const [signer] = await ethers.getSigners();
+  const rpcMumbai = new ethers.providers.JsonRpcProvider(MUMBAI_RPC_URL);
+  const rpcBNB = new ethers.providers.JsonRpcProvider(BSCTESTNET_RPC_URL);
 
-  // $LINK Address
-  // const link = "0x779877A7B0D9E8603169DdbD7836e478b4624789"; // Sepolia
-  const link = "0x326C977E6efc84E512bB9C30f76E30c160eD06FB"; // Mumbai
-  // const link = "0x84b9B910527Ad5C03A9Ca831909E21e236EA7b06"; // BNB
+  var signerMumbai = new ethers.Wallet(`${DEPLOYER_PRIVATE_KEY}`, rpcMumbai);
+  var signerBNB = new ethers.Wallet(`${DEPLOYER_PRIVATE_KEY}`, rpcBNB);
 
-  // Destination Chain
-  // const destinationChain = "16015286601757825753"; // Sepolia
-  // const destinationChain = "12532609583862916517"; // Mumbai
-  const destinationChain = "13264668187771770619"; // BNB
+  const ContracMumbai = await ethers.getContractAt(
+    "Overswap",
+    OVERSWAP_MUMBAI as string,
+    signerMumbai
+  );
 
-  // Last Mock Deployed
-  const mockERC721_sepolia = "0x416AbcB8217721C6a12f776419aaFc27391eA5c3"; // Sepolia
-  const mockERC721_mumbai = "0x2De6d72A010c81817544773615923F2765c4C04f"; // Mumbai
-  const mockERC721_bnb = "0xB7c49b0d0449796031d197AAF3CFaa2a49c63865"; // BNB
+  const ContractBNB = await ethers.getContractAt(
+    "Overswap",
+    OVERSWAP_BNB as string,
+    signerBNB
+  );
 
-  // Official Deployed
-  // const Overswap = "0x5983E90123ccC820b96FAa34153530eF6621a17C"; // Sepolia
-  const Overswap = "0x654008B3b8FE0A715D03Ca63299aE69086a5dab3"; // Mumbai
-  //   const Overswap = "0x2627d6b68Eddc70A9A255bC46c0277433e87E8A2"; // BNB
+  // Check las message received
+  var response = await ContracMumbai.lastReceivedMessageId();
+  console.log(
+    "Received response: %s \n On address: %s",
+    response,
+    ContracMumbai.address
+  );
 
-  // Last deployed contract address
-  const Contract = await ethers.getContractAt("Overswap", Overswap, signer);
+  var response = await ContractBNB.lastReceivedMessageId();
+  console.log(
+    "Received response: %s \n On address: %s",
+    response,
+    ContractBNB.address
+  );
 
-  // Check
-  const response = await Contract.lastReceivedMessageId();
-  console.log(response);
-  console.log("aloo");
+  // Check unlock steps amount
+  var proof =
+    "0x89ba2a82aad4b4f09b6843674b3e4a857b24e9e212bc836cfbcf7f9956de50ab";
+  var response = await ContracMumbai.getUnlockSteps(proof);
+  console.log("Received response: %s on Mumbai", response);
 
-  await Contract.redeem();
+  var response = await ContractBNB.getUnlockSteps(proof);
+  console.log("Received response: %s on BNB", response);
 }
 
 main().catch((error) => {
