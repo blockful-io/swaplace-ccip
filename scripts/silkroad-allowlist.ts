82ab8055ab24320bb2ce8be9a8f33cd935fa3d27
0xneves <asguilift@hotmail.com>
2023-12-16 05:37:55 -0300

    refactor: allowlist using --network

--- scripts/silkroad-allowlist.ts
+++ scripts/silkroad-allowlist.ts
@@ -1,70 +1,31 @@
+import { abi } from "../artifacts/contracts/Swaplace.sol/Swaplace.json";
 import { ethers } from "hardhat";
-import dotenv from "dotenv";
-import { mumbai_chain_selector, bnb_chain_selector } from "../scripts/utils";
-dotenv.config();
+import { getChainData } from "./utils";
 
-const {
-  DEPLOYER_PRIVATE_KEY,
-  MUMBAI_RPC_URL,
-  BSCTESTNET_RPC_URL,
-  SWAPLACE_MUMBAI,
-  SWAPLACE_BNB,
-} = process.env;
+export async function contractConfiguration(
+  signer: any,
+  destinationChain: any,
+  destinationContract: any
+) {
+  // Get contract address from .env
+  const chainData = await getChainData();
 
-export async function deployCCIP() {
-  const rpcMumbai = new ethers.providers.JsonRpcProvider(MUMBAI_RPC_URL);
-  const rpcBNB = new ethers.providers.JsonRpcProvider(BSCTESTNET_RPC_URL);
+  // Get contract instance
+  const Swaplace = new ethers.Contract(chainData.address, abi, signer);
 
-  var signerMumbai = new ethers.Wallet(`${DEPLOYER_PRIVATE_KEY}`, rpcMumbai);
-  var signerBNB = new ethers.Wallet(`${DEPLOYER_PRIVATE_KEY}`, rpcBNB);
-
-  const ContractMumbai = await ethers.getContractAt(
-    "Swaplace",
-    SWAPLACE_MUMBAI as string,
-    signerMumbai
-  );
-
-  const ContractBNB = await ethers.getContractAt(
-    "Swaplace",
-    SWAPLACE_BNB as string,
-    signerBNB
-  );
-
-  var tx = await ContractMumbai.connect(signerMumbai).allowlistSender(
-    bnb_chain_selector,
-    ContractBNB.address,
-    { gasLimit: 4000000 }
+  // Set the chain selector and contract that are allowed to interact with this Swaplace
+  var tx = await Swaplace.allowlistSender(
+    destinationChain,
+    destinationContract,
+    {
+      gasLimit: 4000000,
+    }
   );
   await tx.wait();
 
-  var tx = await ContractBNB.connect(signerBNB).allowlistSender(
-    mumbai_chain_selector,
-    ContractMumbai.address,
-    { gasLimit: 4000000 }
-  );
-  await tx.wait();
-
-  var res = await ContractMumbai.allowlistSenders(bnb_chain_selector);
-  var res = await ContractBNB.allowlistSenders(mumbai_chain_selector);
-
   console.log(
-    "\nThe Mumbai Address that was allowlist",
-    ContractMumbai.address
-  );
-  console.log("Destination Chain that was saved", mumbai_chain_selector);
-  console.log("\nThe BNB Address that was allowlist", ContractBNB.address);
-  console.log("Destination Chain that was saved", bnb_chain_selector);
-  console.log(
-    "\nAllowlisted Mumbai chain on BNB, the allowed Swaplace address on Mumbai -> %s",
-    res
-  );
-  console.log(
-    "Allowlisted BNB chain on Mumbai, the allowed Swaplace address on BNB -> %s",
-    res
+    "\nAllowed contract %s in chain %s to interact with Swaplace",
+    destinationContract,
+    destinationChain
   );
 }
-
-deployCCIP().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
