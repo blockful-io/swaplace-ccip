4b0d8c7159e941ac4e546313bae9eeb1ee2e08c9
0xneves <asguilift@hotmail.com>
2023-12-17 10:33:56 -0300

    refactor: mint function is now more dynamic
    - will always mint a new token based on the total supply
    - will fetch the signer from the chain selector passed as argument

--- scripts/mint/silkroad-mint.ts
+++ scripts/mint/silkroad-mint.ts
@@ -2,23 +2,29 @@ import { abi } from "../../artifacts/contracts/MockERC721.sol/MockERC721.json";
 import { ethers } from "hardhat";
 import { getMockData } from "../utils";
 
-async function mint(receiver: any, tokenId: any, signer: any) {
+export async function mint(receiver: any, chainSelector: any) {
   // Get contract address from .env
-  const mock = await getMockData();
+  const mock = await getMockData(chainSelector);
+
+  // Get the signer based on the chain selector
+  const RPC = new ethers.providers.JsonRpcProvider(mock.rpcUrl);
+  const signer = new ethers.Wallet(`${process.env.DEPLOYER_PRIVATE_KEY}`, RPC);
 
   // Create contract instance
   const Contract = new ethers.Contract(mock.address, abi, signer);
 
-  // Mint to receiver
-  const tx = await Contract.mintTo(receiver, tokenId);
+  // Mint to receiver a new token ID
+  const totalSupply = await Contract.totalSupply();
+  const lastTokenId = Number(totalSupply) + 1;
+  const tx = await Contract.mintTo(receiver, lastTokenId);
   await tx.wait();
 
   console.log(
-    "Minted 1 NFT of ID %s to %s in chain %s",
-    tokenId,
+    "Minted 1 NFT of ID %s to %s in chain %s\n",
+    lastTokenId,
     receiver,
     mock.chainId
   );
 
-  return tx;
+  return lastTokenId;
 }
